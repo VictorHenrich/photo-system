3a1869759e2199015bf15598910c55ba8a84b2df
VictorHenrich <victorhenrich993@gmail.com>
2024-07-18 17:50:04 -0300

    Aplicando melhorias

--- server/instances.py
+++ server/instances.py
@@ -1,10 +1,22 @@
 from server.api import Api
 
 from server.event import AppEvents
-from utils.constants import API_HOST, API_PORT
+from utils.constants import (
+    API_HOST,
+    API_PORT,
+    SWAGGER_API_VERSION,
+    SWAGGER_API_TITLE,
+    SWAGGER_API_DESCRIPTION,
+)
 
 
 class AppInstances:
-    api: Api = Api(host=API_HOST, port=API_PORT)
+    api: Api = Api(
+        host=API_HOST,
+        port=API_PORT,
+        title=SWAGGER_API_TITLE,
+        description=SWAGGER_API_DESCRIPTION,
+        version=SWAGGER_API_VERSION,
+    )
 
     events: AppEvents = AppEvents()
