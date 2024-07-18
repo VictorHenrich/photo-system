a7bd31286242012a9c01a3ec38993c610b886f3c
VictorHenrich <victorhenrich993@gmail.com>
2024-07-18 15:15:28 -0300

    Aplicando ultimas correções

--- controllers/index.py
+++ controllers/index.py
@@ -1,9 +1,11 @@
+from typing import Union
 from fastapi import UploadFile, File
 
 from server.instances import AppInstances
 from utils.constants import INDEX_ENDPOINT_URL, RESIZE_PICTURE_ENDPOINT_URL
-from utils.responses import JSONSuccessResponse
+from utils.responses import JSONSuccessResponse, JSONErrorResponse
 from utils.entities import IndexEntity
+from utils.exceptions import ServiceError
 from services.file import FileService
 
 
@@ -17,15 +19,19 @@ async def index() -> JSONSuccessResponse[IndexEntity]:
 @AppInstances.api.post(RESIZE_PICTURE_ENDPOINT_URL)
 async def resize_image(
     body: UploadFile = File(), width: str = "300", height: str = "300"
-) -> JSONSuccessResponse:
+) -> Union[JSONSuccessResponse, JSONErrorResponse]:
     file_service: FileService = FileService()
 
     content: bytes = await body.read()
 
     filename: str = body.filename or ""
 
-    file_service.send_to_resize_picture(
-        content=content, filename=filename, width=int(width), height=int(height)
-    )
+    try:
+        file_service.send_to_resize_picture(
+            content=content, filename=filename, width=int(width), height=int(height)
+        )
+
+    except ServiceError as error:
+        return JSONErrorResponse(detail=str(error))
 
     return JSONSuccessResponse(content=None)
