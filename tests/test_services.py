1f334aa1710340791bce6480800cd9a7376d450d
VictorHenrich <victorhenrich993@gmail.com>
2024-07-19 11:49:45 -0300

    Finalizando testes

--- tests/test_services.py
+++ tests/test_services.py
@@ -1,9 +1,13 @@
 from unittest import TestCase
+from unittest.mock import Mock, patch
 from pathlib import Path
 from io import BytesIO
+from pydantic import ValidationError
 
+from server.event import AppProducer
 from services.file import FileService
-from utils.constants import ASSETS_PATH
+from utils.constants import ASSETS_PATH, PICTURE_RESIZING_TOPIC
+from utils.entities import ResizingEntity
 
 
 class ServicesTestCase(TestCase):
@@ -64,16 +68,68 @@ class ServicesTestCase(TestCase):
                     picture=picture, height=height, width=width, format=format
                 )
 
-    def test_send_to_resize_picture_with_success(self) -> None:
+    @patch("services.file.ResizingEntity", spec=ResizingEntity)
+    @patch("services.file.AppProducer", spec=AppProducer)
+    def test_send_to_resize_picture_with_success(
+        self, app_producer_class: Mock, resizing_entity_class: Mock
+    ) -> None:
+        app_producer_instance: Mock = Mock()
+
+        app_producer_instance.send.return_value = None
+
+        resizing_entity_instance: Mock = Mock(width=100, height=100, filename="")
+
+        resizing_entity_instance.model_dump_json.return_value = ""
+
+        resizing_entity_class.return_value = resizing_entity_instance
+
+        app_producer_class.return_value = app_producer_instance
+
         file_service: FileService = FileService()
 
-        file_path: Path = ASSETS_PATH / "test.jpg"
+        file_service.send_to_resize_picture(
+            content=bytes(),
+            filename=resizing_entity_instance.filename,
+            width=resizing_entity_instance.width,
+            height=resizing_entity_instance.height,
+        )
 
-        width: int = 100
+        resizing_entity_class.assert_called()
 
-        height: int = 100
+        app_producer_class.assert_called()
 
-        with open(file_path, mode="rb+") as file:
+        resizing_entity_instance.model_dump_json.assert_called_once()
+
+        app_producer_instance.send.assert_called_once_with(
+            PICTURE_RESIZING_TOPIC, bytes()
+        )
+
+    @patch("services.file.AppProducer", spec=AppProducer)
+    def test_send_to_resize_picture_with_validation_error(self, _: Mock) -> None:
+        wrong_object: Mock = Mock(
+            content=bytes(), filename="", width="wrong_width", height="wrong_height"
+        )
+
+        file_service: FileService = FileService()
+
+        with self.assertRaises(ValidationError):
             file_service.send_to_resize_picture(
-                content=file.read(), filename=file.name, width=width, height=height
+                content=wrong_object.content,
+                filename=wrong_object.filename,
+                width=wrong_object.width,
+                height=wrong_object.height,
+            )
+
+    @patch("services.file.AppProducer", spec=AppProducer)
+    def test_send_to_resize_picture_with_type_error(self, _: Mock) -> None:
+        wrong_object: Mock = Mock(content=object(), filename="")
+
+        file_service: FileService = FileService()
+
+        with self.assertRaises(TypeError):
+            file_service.send_to_resize_picture(
+                content=wrong_object.content,
+                filename=wrong_object.filename,
+                width=wrong_object.width,
+                height=wrong_object.height,
             )
